84686717eb6de32665d8bd81c9f9f913d7d497b5
edolf <ngufeel@gmail.com>
2021-08-17 20:00:14 +0700

    buang semua sampah

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
-sudo apt update -y && sudo apt upgrade -y && sudo timedatectl set-timezone Asia/Jakarta && curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt-get install -y nodejs && npm install -y && npm audit fix -y && sudo apt-get install build-essential -y && sudo npm i pkg -g && sudo npm i nexe -g && sudo apt-get install libjsoncpp-dev -y
+#!/bin/bash
+
+function main() {
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo timedatectl set-timezone Asia/Jakarta
+  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  npm install -y
+  npm audit fix -y
+  sudo apt-get install build-essential -y
+  sudo npm i pkg -g
+  sudo npm i nexe -g
+}
+
+main
