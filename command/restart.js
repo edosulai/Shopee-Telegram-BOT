65984b621508328bd77df9595f61b04a1a15da31
edolf <ngufeel@gmail.com>
2021-09-12 10:36:10 +0700

    restart command completed

--- command/restart.js
+++ command/restart.js
@@ -9,7 +9,7 @@ module.exports = async function (ctx) {
   let user = ctx.session
   user.commands = getCommands(ctx.message.text)
 
-  exec(`${user.commands['-update'] ? 'git pull && ' : ''}touch index.js`, (error, stdout, stderr) => {
+  exec(`${user.commands['-update'] ? 'git reset --hard && git pull && ' : ''}touch index.js`, (error, stdout, stderr) => {
     if (error) return sendReportToDev(ctx, new Error(error.message))
     return ctx.reply(`Restart ${user.commands['-update'] ? 'dan Update Code ' : ''}Server Telah Berhasil`)
   });
